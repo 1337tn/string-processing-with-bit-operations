6247b350132b6fb77798ced5144711086802fef6
Tanya Lyubimaya <tatyana.lyubimaya.work@gmail.com>
2023-02-18 19:38:46 +0300

    🙈⬆️  Added .gitignore, upgraded dependencies

--- bitoperations.go
+++ bitoperations.go
@@ -1,6 +1,7 @@
 package bitoperations
 
 import (
+	"errors"
 	"strings"
 )
 
@@ -11,13 +12,36 @@ const (
 	REV
 )
 
-func ProcessString(str string, conf byte) string {
+func SameSign(a, b int) bool {
+	// sameSign checks whether numbers a and b have the same sign
+	return a^b >= 0
+}
+
+func SetNthBit(num, pos int) int {
+	// setNthBit sets bit on position pos in num
+	return num | (1 << (pos - 1))
+}
+
+func UnsetNthBit(num, pos int) int {
+	// unsetNthBit unsets bit on position pos in num
+	return num &^ (1 << (pos - 1))
+}
+
+func GetNthBit(num, pos int) int {
+	// getNthBit returns bit on position pos in num
+	return num & (1 << (pos - 1))
+}
+
+func processString(str string, conf byte) (string, error) {
 	// processString changes string str according to configuration conf:
 	// UPPER - make str uppercase
 	// LOWER - make str lowercase
 	// CAP - capitalize first letter of each word in str
 	// REV - reverse the letters order in str
 	// conf can set multiple parameters as in conf = REV|CAP
+	if len(str) == 0 {
+		return str, errors.New("Empty string")
+	}
 	rev := func(s string) string {
 		runes := []rune(s)
 		n := len(runes)
@@ -53,25 +77,5 @@ func ProcessString(str string, conf byte) string {
 	if (conf & REV) != 0 {
 		str = rev(str)
 	}
-	return str
-}
-
-func SameSign(a, b int) bool {
-	// sameSign checks whether numbers a and b have the same sign
-	return a^b >= 0
-}
-
-func SetNthBit(num, pos int) int {
-	// setNthBit sets bit on position pos in num
-	return num | (1 << (pos - 1))
-}
-
-func UnsetNthBit(num, pos int) int {
-	// unsetNthBit unsets bit on position pos in num
-	return num &^ (1 << (pos - 1))
-}
-
-func GetNthBit(num, pos int) int {
-	// getNthBit returns bit on position pos in num
-	return num & (1 << (pos - 1))
+	return str, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module awesomeProject1
+module bitoperations
 
-go 1.18
+go 1.20
